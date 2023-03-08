2b1cc6c8dd3063741e1bfbace97b6cbfd2237ca7
Harrieson <harriemburu@gmail.com>
2023-03-08 23:50:05 +0300

    Building Models

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./pages/**/*.{js,ts,tsx,tsx}",
+        "./components/**/*.{js,ts,tsx,jsx}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
